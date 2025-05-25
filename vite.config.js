f7ccb656d34220e4f89d2e6ac93a62d6d606fef8
Khang <khangnguyend16@gmail.com>
2025-05-26 00:24:40 +0700

    4

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  // base: './'
   resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
-  },
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
+    alias: [{ find: "~", replacement: "/src" }],
   },
 });
